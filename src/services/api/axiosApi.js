761641947c0ec15d370c3da59499f6f1c4d82f4a
anon <iqbalshl08@gmail.com>
2026-08-12 11:32:13 +0700

    update backend

--- src/services/api/axiosApi.js
+++ src/services/api/axiosApi.js
@@ -6,6 +6,22 @@ const axiosApi = axios.create({
         "Content-Type": 'application/json',    
     },
     
-})
+});
+
+axiosApi.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if(token) {
+            config.headers.Authorization = 'Bearer ${token}';
+        }
+
+        return config;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+)
 
 export default axiosApi;
